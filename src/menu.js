de20a8e2bc51dd5d26cfdc898cb2549e3fd003d5
milohill <116620023+grimhilt7@users.noreply.github.com>
2023-02-03 12:43:11 +0900

    rearrange style code

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,6 @@
+import menuOne from './images/menu-01.jpg';
+import menuTwo from './images/menu-02.jpg';
+
 export default function addMenu() {
   const wrapper = document.createElement('div');
   wrapper.classList.add('menu');
